5ea73dbf6e34347de4493899c1fc9e21071e96ba
Sitro23 <73548649+Sitro23@users.noreply.github.com>
2021-01-06 19:58:00 +0100

    Update wyzwalacze.sql

--- Projekt/wyzwalacze.sql
+++ Projekt/wyzwalacze.sql
@@ -12,3 +12,18 @@ CREATE FUNCTION hello (s CHAR(20)) RETURNS CHAR(50) DETERMINISTIC RETURN CONCAT(
 --+----------------+
 --| Hello, world!  |
 --+----------------+
+                              
+                              DELIMITER // 
+CREATE TRIGGER wyzwalacz1 
+BEFORE INSERT ON ksiazki for each row 
+BEGIN set new.opis="Wkrótce więcej inforamacji";END// 
+
+DELIMITER $$
+CREATE TRIGGER wyzywalacz2
+BEFORE INSERT ON ksiazki for each row 
+BEGIN if new.isbn>900000 then set new.isbn=9999999999;
+END if;
+END $$                              
+                              
+                              
+                              
